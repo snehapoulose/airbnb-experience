ea6b44ab4b68bf1a8f2438852c906254089b86ff
Sneha Poulose <snehapoulose1999@gmail.com>
2022-01-06 13:37:46 +0530

    -created a airbnb experiences clone.

---
+++ src/components/Card.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "../App.css";
+function Card(props) {
+  return (
+    <div className="card">
+      <img
+        src={`../images/${props.coverImg}`}
+        alt="book"
+        className="card-image"
+      />
+      <div className="card-info">
+        <img src="../images/star.png" className="card-rating" />
+        <p>{props.stats.rating}</p>
+        <p>({props.stats.reviewCount}) • </p>
+        <p>{props.location}</p>
+      </div>
+      <p className="card-title">{props.title}</p>
+      <p className="card-price">From ${props.price} / person </p>
+    </div>
+  );
+}
+export default Card;
---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "../App.css";
+function Navbar() {
+  return (
+    <div className="navbar">
+      <img src="../images/airbnb-logo.png" alt="airbnb-logo" className="navbar-logo" />
+    </div>
+  );
+}
+export default Navbar;
